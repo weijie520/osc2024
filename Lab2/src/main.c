9dbceefb4028762260c89bd5195fad466f4bfd28
weijie520 <wabbywu260590@gmail.com>
2024-03-13 12:17:27 +0800

    add dtb(flatten device tree)

--- Lab2/src/main.c
+++ Lab2/src/main.c
@@ -2,10 +2,22 @@
 #include "shell.h"
 #include "string.h"
 #include "heap.h"
+#include "devicetree.h"
+#include "initrd.h"
 
-void main(){
+
+void main(void* dtb_ptr){
     uart_init();
-    uart_sends("Hello world\n");
 
+    uart_sends("\nDtb loaded at ");
+    uart_sendh((uint32_t *)dtb_ptr);
+
+    uart_sends("\nbefore: ");
+    uart_sendh(get_initrd());
+    fdt_traverse(dtb_ptr, initramfs_callback);
+    uart_sends("\nafter: ");
+    uart_sendh(get_initrd());
+    uart_sends("\n");
+  
     shell_exec();
 }
