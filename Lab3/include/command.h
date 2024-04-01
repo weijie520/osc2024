336494652144e4bd95cea97b8c0b1bfb85432f11
weijie520 <wabbywu260590@gmail.com>
2024-04-01 10:06:45 +0800

    1. update command function to receive args 2. add exec(), settimeout(), and async_test() function

--- Lab3/include/command.h
+++ Lab3/include/command.h
@@ -5,20 +5,23 @@
 #define PM_RSTC     (volatile unsigned int*)0x3F10001c
 #define PM_WDOG     (volatile unsigned int*)0x3F100024
 
-#define MAX_COMM_NUM 6
+#define MAX_COMM_NUM 9
 
 typedef struct{
   const char *name;
   const char *description;
-  int (*func)(void);
+  int (*func)(void**);
 }Command;
 
-int help();
-int hello();
-int reboot();
-int lshw();
-int ls();
-int cat();
+int help(void**);
+int hello(void**);
+int reboot(void**);
+int lshw(void**);
+int ls(void**);
+int cat(void**);
+int exec(void**);
+int setTimeout(void**);
+int test(void**);
 
 extern Command commands[];
 
--- Lab3/src/command.c
+++ Lab3/src/command.c
@@ -2,6 +2,9 @@
 #include "mini_uart.h"
 #include "mailbox.h"
 #include "initrd.h"
+#include "string.h"
+#include "heap.h"
+#include "timer.h"
 
 Command commands[] = {
     {"help", "print all available commands.", help},
@@ -9,14 +12,13 @@ Command commands[] = {
     {"lshw", "show some hardware information.", lshw},
     {"reboot", "reboot the device.", reboot},
     {"ls", "list directory contents", ls},
-    {"cat", "print on the standard output", cat}
+    {"cat", "print on the standard output", cat},
+    {"exec", "excution a user program", exec},
+    {"settimeout", "prints MESSAGE after SECONDS with the current time and the command executed time", setTimeout},
+    {"test", "test async read and write", test}
 };
 
-// enum Type inputToType(const char* command){
-
-// }
-
-int help(){
+int help(void* args[]){
   for (int i = 0; i < MAX_COMM_NUM; i++)
   {
     uart_sends(commands[i].name);
@@ -27,54 +29,78 @@ int help(){
   return 0;
 }
 
-int hello(){
+int hello(void* args[]){
   uart_sends("Hello world!\n");
   return 0;
 }
 
-int lshw(){
+int lshw(void* args[]){
   get_board_revision();
   get_arm_memory();
   return 0;
 }
 
-int reboot(){
-  // uart_writeS("Re\n");
+int reboot(void* args[]){
+  uart_sends("Rebooting...\n");
   *PM_RSTC = (PM_PASSWORD | 0x20);
   *PM_WDOG = (PM_PASSWORD | 20);
   return 0;
 }
 
-int ls(){
+int ls(void* args[]){
   initrd_list();
   return 0;
 }
 
-int cat(){
-  uart_sends("Filename: ");
-  char filename[257];
-  char *tmp = filename;
-  for (int i = 0; i < 256; i++)
-  {
-    *tmp = uart_recv();
-    uart_sendc(*tmp);
-    if (*tmp == 127){
-      i--;
-      if(i >= 0){
-        i--;
-        *tmp-- = 0;
-        uart_sendc('\b');
-        uart_sendc(' ');
-        uart_sendc('\b');
-      }
-    }
-    else if (*tmp == '\n'){
-      break;
-    }
-    else tmp++;
+int cat(void* args[]){
+  if(!args){
+    return -1;
   }
-  *tmp = '\0';
-  
+  char filename[257];
+  strcpy(filename, args[1]);
+
   initrd_cat(filename);
+  return 0;
+}
+
+int exec(void* args[]){
+  if(!args){
+    return -1;
+  }
+  char filename[257];
+  strcpy(filename, args[1]);
+  void* exec_data = fetch_exec(filename);
+
+  if(exec_data){
+
+    void *stack = simple_malloc(STACK_SIZE);
+    void *stack_top = stack + STACK_SIZE;
+
+    asm volatile(
+      "mov x4, 0x3c0;"
+      "msr spsr_el1, x4;"
+      "msr elr_el1, %0;"
+      "msr sp_el0, %1;"
+      "eret;" :: "r" (exec_data), "r" (stack_top) // :: is for input; : for output
+    );
+  }
+  else{
+    uart_sends("exec: ");
+    uart_sends(filename);
+    uart_sends(" No such file or directory\n");
+  }
+  return 0;
+}
+
+int setTimeout(void* args[]){
+  if(!args){
+    return -1;
+  }
+  set_timeout((char*)args[1], atoi(args[2]));
+  return 0;
+}
+
+int test(void* args[]){
+  uart_async_test();
   return 0;
 }
