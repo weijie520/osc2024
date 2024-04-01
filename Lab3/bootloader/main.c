e8dea438483d9adf6b585f641676724752d17104
weijie520 <wabbywu260590@gmail.com>
2024-04-01 10:23:01 +0800

    delete the uart_sends before recv size

--- Lab3/bootloader/main.c
+++ Lab3/bootloader/main.c
@@ -6,21 +6,12 @@ void main(){
   
   uart_init();
   
-  // uint8_t size_bytes[4];
   uint32_t kernel_size = 0;
 
-  uart_sends("\nWait for loading...\n");
-
-
-  // while(1)
-  //   uart_send('1');
   for(int i = 0; i < 4; i++){
-    // size_bytes[i] = uart_recv();
-    kernel_size = (kernel_size << 8) | (uart_recv() & 0xff);
-    // char c = uart_recv();
-    // uart_send(c);
+    kernel_size = (kernel_size << 8) | (uart_recv());
   }
-  // kernel_size = (size_bytes[0]<<24)|(size_bytes[1]<<16)|(size_bytes[2]<<8)|size_bytes[3];
+
   uart_sends("The kernel size: ");
   uart_sendh(kernel_size);
   uart_sendc('\n');
