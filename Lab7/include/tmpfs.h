96e755ae3f2f2ff22ed0181b79860a12cef62223
weijie520 <wabbywu260590@gmail.com>
2024-06-24 10:37:21 +0800

    add tmpfs for rootfs

---
+++ Lab7/include/tmpfs.h
@@ -0,0 +1,33 @@
+#ifndef __TMPFS_H__
+#define __TMPFS_H__
+
+#include "vfs.h"
+
+#define TMPFS_MAX_NAME_LEN 15
+#define TMPFS_MAX_ENTRIES 16
+#define TMPFS_MAX_FILE_SIZE 4096
+
+struct tmpfs_node
+{
+  char name[TMPFS_MAX_NAME_LEN + 1];
+  int is_dir;
+  unsigned int size;
+  char *data;
+  struct vnode *entries[TMPFS_MAX_ENTRIES];
+};
+
+int register_tmpfs();
+int tmpfs_write(struct file *file, const void *buf, unsigned int len);
+int tmpfs_read(struct file *file, void *buf, unsigned int len);
+int tmpfs_open(struct vnode *node, struct file **file);
+int tmpfs_close(struct file *file);
+
+int tmpfs_lookup(struct vnode *dir_node, struct vnode **target, const char *component_name);
+int tmpfs_create(struct vnode *dir_node, struct vnode **target, const char *component_name);
+int tmpfs_mkdir(struct vnode *dir_node, struct vnode **target, const char *component_name);
+
+int tmpfs_setup_mount(struct filesystem *fs, struct mount *mount);
+
+int tmpfs_lookup(struct vnode *dir_node, struct vnode **target, const char *component_name);
+
+#endif
---
+++ Lab7/src/tmpfs.c
@@ -0,0 +1,164 @@
+#include "tmpfs.h"
+#include "vfs.h"
+#include "string.h"
+#include "memory.h"
+
+int register_tmpfs()
+{
+  struct filesystem *fs = (struct filesystem *)kmalloc(sizeof(struct filesystem));
+  fs->name = "tmpfs";
+  fs->setup_mount = tmpfs_setup_mount;
+  return register_filesystem(fs);
+}
+
+int tmpfs_write(struct file *file, const void *buf, unsigned int len)
+{
+  struct tmpfs_node *node = (struct tmpfs_node *)file->vnode->internal;
+
+  if (file->f_pos + len > TMPFS_MAX_FILE_SIZE)
+    len = TMPFS_MAX_FILE_SIZE - file->f_pos;
+
+  memcpy(node->data + file->f_pos, (void *)buf, len);
+  file->f_pos += len;
+  if (node->size < file->f_pos)
+    node->size = file->f_pos;
+
+  return len;
+}
+
+int tmpfs_read(struct file *file, void *buf, unsigned int len)
+{
+  struct tmpfs_node *node = (struct tmpfs_node *)file->vnode->internal;
+
+  if (file->f_pos + len > node->size)
+    len = node->size - file->f_pos;
+
+  memcpy(buf, node->data + file->f_pos, len);
+  file->f_pos += len;
+
+  return len;
+}
+
+int tmpfs_open(struct vnode *node, struct file **file)
+{
+  (*file)->vnode = node;
+  (*file)->f_pos = 0;
+  (*file)->f_ops = node->f_ops;
+  return 0;
+}
+
+int tmpfs_close(struct file *file)
+{
+  kfree(file);
+  return 0;
+}
+
+long tmpfs_lseek64(struct file *file, long offset, int whence)
+{
+  long new_pos = 0;
+  switch (whence)
+  {
+  case SEEK_SET:
+    new_pos = offset;
+    break;
+  case SEEK_CUR:
+    new_pos = file->f_pos + offset;
+    break;
+  // case SEEK_END:
+  //   new_pos = (int)file->vnode->internal->size;
+  //   break;
+  default:
+    return -1;
+  }
+
+  if (new_pos < 0)
+    return -1;
+
+  file->f_pos = new_pos;
+  return new_pos;
+}
+
+int tmpfs_lookup(struct vnode *dir_node, struct vnode **target, const char *component_name)
+{
+  struct tmpfs_node *dir = (struct tmpfs_node *)dir_node->internal;
+  for (int i = 0; i < TMPFS_MAX_ENTRIES; i++)
+  {
+    if (dir->entries[i] != 0 && strcmp(((struct tmpfs_node *)dir->entries[i]->internal)->name, component_name) == 0)
+    {
+      *target = dir->entries[i];
+      return 0;
+    }
+  }
+  return -1;
+}
+
+int tmpfs_create(struct vnode *dir_node, struct vnode **target, const char *component_name)
+{
+  struct tmpfs_node *dir = (struct tmpfs_node *)dir_node->internal;
+  for (int i = 0; i < TMPFS_MAX_ENTRIES; i++)
+  {
+    if (dir->entries[i] == 0)
+    {
+      struct tmpfs_node *new_node = (struct tmpfs_node *)kmalloc(sizeof(struct tmpfs_node));
+      strcpy(new_node->name, component_name);
+      new_node->is_dir = 0;
+      new_node->size = 0;
+      new_node->data = (char *)kmalloc(TMPFS_MAX_FILE_SIZE);
+      memset(new_node->data, 0, TMPFS_MAX_FILE_SIZE);
+      for (int j = 0; j < TMPFS_MAX_ENTRIES; j++)
+        new_node->entries[j] = 0;
+
+      dir->entries[i] = *target;
+      *target = kmalloc(sizeof(struct vnode));
+      (*target)->internal = (void *)new_node;
+      (*target)->f_ops = dir_node->f_ops;
+      (*target)->v_ops = dir_node->v_ops;
+      (*target)->mount = 0;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+int tmpfs_mkdir(struct vnode *dir_node, struct vnode **target, const char *component_name)
+{
+  struct tmpfs_node *dir = (struct tmpfs_node *)dir_node->internal;
+  for (int i = 0; i < TMPFS_MAX_ENTRIES; i++)
+  {
+    if (dir->entries[i] == 0)
+    {
+      struct tmpfs_node *new_node = (struct tmpfs_node *)kmalloc(sizeof(struct tmpfs_node));
+      strcpy(new_node->name, component_name);
+      new_node->is_dir = 1;
+      new_node->size = 0;
+      new_node->data = 0;
+      for (int j = 0; j < TMPFS_MAX_ENTRIES; j++)
+        new_node->entries[j] = 0;
+
+      dir->entries[i] = *target;
+      *target = (struct vnode *)kmalloc(sizeof(struct vnode));
+      (*target)->internal = (void *)new_node;
+      (*target)->f_ops = dir_node->f_ops;
+      (*target)->v_ops = dir_node->v_ops;
+      (*target)->mount = 0;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+int tmpfs_setup_mount(struct filesystem *fs, struct mount *mount)
+{
+  struct tmpfs_node *root = (struct tmpfs_node *)kmalloc(sizeof(struct tmpfs_node));
+  strcpy(root->name, "/");
+  root->is_dir = 1;
+  root->size = 0;
+  root->data = 0;
+  for (int i = 0; i < TMPFS_MAX_ENTRIES; i++)
+    root->entries[i] = 0;
+
+  mount->root = (struct vnode *)root;
+  mount->root->mount = mount;
+
+  return 0;
+}
